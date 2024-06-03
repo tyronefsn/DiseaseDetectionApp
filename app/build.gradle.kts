e8ff9ac7bfce91f2f61cb44e61f74b8908eb2588
tyronefsn <tsnagano1@up.edu.ph>
2024-06-03 14:43:50 +0800

    feat: add carview dependency
    using cardview to allow easier implementation of the UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation(libs.tensorflow.lite.support)
     implementation(libs.tensorflow.lite.metadata)
-
+    implementation(libs.cardview)
 
 
 
